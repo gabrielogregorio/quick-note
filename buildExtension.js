65e2bbe9e401852ad3bb90a5efeb28e339fdaf8b
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-10-15 19:27:35 -0300

    feat: added build extension

--- buildExtension.js
+++ buildExtension.js
@@ -5,3 +5,22 @@ fs.copyFileSync("./README.md", "./dist/README.md");
 fs.copyFileSync("./manifest.json", "./dist/manifest.json");
 fs.cpSync("./styles", "./dist/styles", { recursive: true });
 fs.cpSync("./public", "./dist/public", { recursive: true });
+
+const archiver = require("archiver");
+
+const packageVersion = require("./package.json").version;
+
+const output = fs.createWriteStream(`./release/${packageVersion}.zip`);
+const archive = archiver("zip", {
+  zlib: { level: 9 },
+});
+
+archive.on("error", function (err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+archive.directory("./dist/", false);
+
+archive.finalize();
